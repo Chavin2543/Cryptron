057f47e5f25dc380f05e832b52014184e98c9541
Chavin2543 <rungallday@hotmail.com>
2022-02-12 18:25:43 +0700

    Initial Commit

---
+++ Cryptron/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cryptron
+//
+//  Created by chavin Panicharoen on 12/2/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cryptron/CryptronApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptronApp.swift
+//  Cryptron
+//
+//  Created by chavin Panicharoen on 12/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct CryptronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
